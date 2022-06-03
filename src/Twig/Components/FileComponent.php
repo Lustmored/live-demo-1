3bd4fbf7fdcccd024df24a31c7b33f8fc9d84c98
Jakub Caban <kuba.iluvatar@gmail.com>
2022-06-03 13:31:58 +0200

    Adapt to live-component changes

--- src/Twig/Components/FileComponent.php
+++ src/Twig/Components/FileComponent.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Twig\Components;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveFileArg;
@@ -15,13 +16,23 @@ class FileComponent
     use DefaultActionTrait;
 
     #[LiveProp]
-    public array $files = [];
+    public array $autoFiles = [];
+
+    #[LiveProp]
+    public array $manualFiles = [];
 
     #[LiveAction]
     public function test(
-        #[LiveFileArg(name: 'form[file]', multiple: true)]
-        array $files
+        #[LiveFileArg]
+        ?UploadedFile $manualFiles = null,
+        #[LiveFileArg]
+        ?array $autoFiles = null
     ): void{
-        $this->files = $files;
+        if ($manualFiles) {
+            $this->manualFiles = [$manualFiles];
+        }
+        if ($autoFiles) {
+            $this->autoFiles = $autoFiles;
+        }
     }
 }
