85dfd0d1a272193c3686377018c9c4c2ab924a57
Jakub Caban <kuba.iluvatar@gmail.com>
2022-06-10 13:13:05 +0200

    Validate component via ValidatableComponentTrait

--- src/Twig/Components/FileComponent.php
+++ src/Twig/Components/FileComponent.php
@@ -4,35 +4,41 @@ declare(strict_types=1);
 namespace App\Twig\Components;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveFileArg;
-use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
 
 #[AsLiveComponent('file')]
 class FileComponent
 {
     use DefaultActionTrait;
+    use ValidatableComponentTrait;
 
-    #[LiveProp]
+    #[Valid, All([new Image()])]
     public array $autoFiles = [];
 
-    #[LiveProp]
-    public array $manualFiles = [];
+    #[Valid, Image]
+    public ?UploadedFile $manualFile = null;
 
     #[LiveAction]
     public function test(
         #[LiveFileArg]
-        ?UploadedFile $manualFiles = null,
+        ?UploadedFile $manualFile = null,
         #[LiveFileArg]
         ?array $autoFiles = null
     ): void{
-        if ($manualFiles) {
-            $this->manualFiles = [$manualFiles];
+        if ($manualFile) {
+            $this->manualFile = $manualFile;
         }
         if ($autoFiles) {
             $this->autoFiles = $autoFiles;
         }
+
+        $this->validate();
     }
 }
